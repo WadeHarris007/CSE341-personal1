0cb282681106d53aa3ab5d5aa08720cb394e19a7
wadeharris001 <wadeharris001@gmail.com>
2024-02-10 23:23:09 +0200

    auth update 2

---
+++ middleware/validate.js
@@ -0,0 +1,28 @@
+const validator = require('../searchPaths/validate');
+
+const saveCarPartsdetails = (req, res, next) => {
+  const validationRule = {
+    carModel: 'required|string',
+    carType: 'required|string',
+    email: 'required|string',
+    firstName: 'required|string',
+    lastName: 'required|string',
+    partNeeded: 'required|string',
+    yearModel: 'required|string'
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = {
+  saveCarPartsdetails
+};
